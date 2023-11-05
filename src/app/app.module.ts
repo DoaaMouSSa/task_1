6de3cac3de15d4ede3114b5195a9dc931469d94c
DoaaMouSSa <doaamoussa5@gmail.com>
2023-11-05 20:46:51 +0200

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
+// import { ListComponent } from './list/list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CourseComponent } from './course/course.component';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CourseComponent,
+    RatingComponent,
   ],
   imports: [
     BrowserModule,
